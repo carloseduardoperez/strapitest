a18440eec7b1241a791d660d2631d5a326e7363b
Carlos Perez <cadukeson@gmail.com>
2021-01-19 10:06:26 -0300

    First Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8ab55786e8cd17a035eb263ac04539c1'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ef8ce41f-21a2-4ebe-ae15-bf41b0c9adb3'
+};
